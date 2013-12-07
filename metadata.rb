35bc545c50c3d79835c92bbdc21c6028949d36f7
Steven Ellis <steven.ellis@mademedia.co.uk>
2013-12-07 00:14:54 +0000

    Bumped version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pogotc@gmail.com'
 license          'MIT'
 description      'Installs/Configures h5bp-nginx-php'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 
 depends 'nginx', '>= 1.7.0'
